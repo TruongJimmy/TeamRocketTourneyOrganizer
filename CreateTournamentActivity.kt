02af64db4878ed822788ac33590ed4ea495987e7
lewisaandres <68321011+lewisaandres@users.noreply.github.com>
2022-02-10 13:02:58 -0800

    WIP Created "Create Tournament Page" Front end

---
+++ CreateTournamentActivity.kt
@@ -0,0 +1,35 @@
+package com.mongodb.tasktracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class CreateTournamentActivity : AppCompatActivity() {
+    private lateinit var tournamentNameInput: EditText
+    private lateinit var typeOfGameInput: EditText
+    private lateinit var participantInput: EditText
+    private lateinit var locationInput: EditText
+    private lateinit var startTimeInput: EditText
+    private lateinit var tournamentTypeInput: EditText
+    private lateinit var createTourneyButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_tournament)
+
+        tournamentNameInput = findViewById(R.id.tournamentName_input)
+        typeOfGameInput = findViewById(R.id.tournamentGame_input)
+        participantInput = findViewById(R.id.participant_input)
+        locationInput = findViewById(R.id.location_input)
+        startTimeInput = findViewById(R.id.startTime_input)
+        tournamentTypeInput = findViewById(R.id.tournamentType)
+        createTourneyButton = findViewById(R.id.createTournamentButton_Button)
+
+        //Creating the back button for this page supported by the toolbar in xml file
+        setSupportActionBar(findViewById(R.id.toolBar_createTournamentActivity))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+}
